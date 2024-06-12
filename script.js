0aa1f785c93e13c4a91be8cf870f807ba10351a3
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-12 15:52:04 +0200

    creata lista cards dei books

---
+++ script.js
@@ -0,0 +1,69 @@
+const fetchBooks = () => {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => {
+      console.log(response);
+      //contollo che la risposta sia positiva tramite lettura della proprietà .ok
+      if (response.ok) {
+        //restituiamo una nuova promise che è l'operazione svolta dal .json()
+        return response.json();
+      } else {
+        //lanciamo un errore per fare in modo che il programma salti il prossimo .then() per andare a finire nel .catch()
+        throw new Error("Errore nel reperire i dati");
+      }
+    })
+    .then((booksObj) => {
+      //qui siamo sicuri di essere sincronizzati con l'arrivo dei dati, perche questo .then() scatterà solo dopo la risoluzione del .json()
+      //quindi adesso si può usare il dato contentuto nel parametro come ci pare per la DOM MANIPULATION
+      console.log(booksObj);
+
+      const row = document.getElementById("cardContainer");
+
+      booksObj.forEach((book) => {
+        const col = document.createElement("div");
+        col.classList.add("col");
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.classList.add("card-img-top");
+        img.src = book.img;
+        img.alt = book.title;
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.innerText = book.title;
+
+        const cardText = document.createElement("p");
+        cardText.classList.add("card-text");
+        cardText.innerText = `${book.price} €`;
+
+        const button = document.createElement("a");
+        button.classList.add("btn");
+        button.classList.add("btn-primary");
+        button.href = "#";
+        button.innerText = "Scarta";
+        button.addEventListener("click", () => {
+          col.remove();
+        });
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(button);
+
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        col.appendChild(card);
+        row.appendChild(col);
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetchBooks();
+});
